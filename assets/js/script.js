d849c7e1c6cab7bb9926bb61d5910f486060abb6
adnan <adnankaka.0092@gmail.com>
2025-01-24 22:31:53 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,70 @@
 $(document).ready(function () {
-  $('.reviews-slider').slick({
-    infinite: true,
-    cssEase: 'linear',
-    speed: 1000,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 2,
     dots: false,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
+    autoplay: false,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        margin: 10,
+        center: true
       },
-    ],
+      768: {
+        items: 2,
+        margin: 20
+      }
+    }
   });
 
   $('#year').text(new Date().getFullYear());
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const moveContent = () => {
+    const mediaQuery = window.matchMedia("(min-width: 768px) and (max-width: 1100px)");
+    const authorName = document.querySelector(".author-name");
+    const authorMeta = authorName?.nextElementSibling;
+    const socialIcons = document.querySelector(".author-social");
+
+    if (mediaQuery.matches) {
+      let newDiv = document.querySelector(".author-details");
+      if (!newDiv) {
+        newDiv = document.createElement("div");
+        newDiv.classList.add("author-details");
+        const parentDiv = document.querySelector(".author-content");
+        parentDiv.appendChild(newDiv);
+      }
+      let secNew = document.querySelector(".author-infos");
+      if (!secNew) {
+        secNew = document.createElement("div");
+        secNew.classList.add("author-infos");
+        const additionalInfo = document.querySelector(".author-details");
+        additionalInfo.appendChild(secNew);
+      }
+      secNew.appendChild(authorName);
+      secNew.appendChild(authorMeta);
+      newDiv.appendChild(socialIcons);
+    } else {
+      const newDiv = document.querySelector(".author-details");
+      if (newDiv) {
+        newDiv.remove();
+      }
+      const secNew = document.querySelector(".author-infos");
+      if (secNew) {
+        secNew.remove();
+      }
+      const orgParent = document.querySelector(".author-content");
+      orgParent.appendChild(authorName);
+      orgParent.appendChild(authorMeta);
+      orgParent.appendChild(socialIcons);
+    }
+  };
+
+  moveContent();
+  window.addEventListener("resize", moveContent);
+});
+
